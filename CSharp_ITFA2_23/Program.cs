c467546e920badbb7cebdbf4d7c4f3b4b5232fbc
Sebi <sebastian.schueler@sebitech.de>
2023-02-23 16:53:38 +0100

    Taschenrechner + Enums

--- CSharp_ITFA2_23/Program.cs
+++ CSharp_ITFA2_23/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSharp_ITFA2_23
 {
     internal class Program
     {
+
+        enum Kontinent { Europa, Asien, Afrika, Australien, Nord_Amerika, Süd_Amerika };
         static void Main(string[] args)
         {
-            Listen_1b.Start();
+            List<string> elemente = new List<string>() { "Schere", "Stein", "Papier"};
+            elemente.Add("Brunnen");
+            elemente[0] = "Baumstamm";
+
+            List<string> liste = new List<string>() { "Asien", "Europa", "Amerika" };
+            liste.Contains("Nord Amerika");
+            liste.Contains("Nord_Amerika");
+            liste.Contains("Nordamerika");
+
+
+            GetKontinent(Kontinent.Asien);
+        }
+        static void GetKontinent(Kontinent kont)
+        {
+            Random rand = new Random();
+
+            Kontinent k = (Kontinent)rand.Next(0,5);
+            
         }
     }
 }
---
+++ CSharp_ITFA2_23/Taschenrechner.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_ITFA2_23
+{
+    internal class Taschenrechner
+    {
+        public static void Start()
+        {
+            Console.WriteLine("Ihr Ergebnis ist: " + Manage(UserInput()));
+        }
+        static List<string> UserInput()
+        {
+            List<string> stringList = new List<string>();
+            Console.WriteLine("Bitte Rechnung eingeben:");
+            string s = Console.ReadLine();
+
+            string tempNum = "";
+            foreach(var element in s)
+            {
+                if (int.TryParse(element.ToString(), out _))
+                    tempNum += element;
+                else
+                {
+                    stringList.Add(tempNum);
+                    tempNum = "";
+                    stringList.Add(element.ToString());
+                }
+            }
+            stringList.Add(tempNum);
+            return stringList;
+        }
+        static int Manage(List<string> stringList)
+        {
+            if (stringList[1] == "+")
+                return Plus(stringList[0], stringList[2]);
+            if(stringList[1] == "-")
+                return Minus(stringList[0], stringList[2]);
+            if (stringList[1] == "*")
+                return Mal(stringList[0], stringList[2]);
+            if (stringList[1] == "/")
+                return Geteilt(stringList[0], stringList[2]);
+
+            return -1;
+        }
+        static int Plus(string a, string b)
+        {
+            return Convert.ToInt32(a) + Convert.ToInt32(b);
+        }
+        static int Minus(string a, string b)
+        {
+            return Convert.ToInt32(a) - Convert.ToInt32(b);
+        }
+        static int Mal(string a, string b)
+        {
+            return Convert.ToInt32(a) * Convert.ToInt32(b);
+        }
+        static int Geteilt(string a, string b)
+        {
+            return Convert.ToInt32(a) / Convert.ToInt32(b);
+        }
+    }
+}
